dffc20cbe02675270a6145998220eaec9448ee66
ernest-tyunyan <100211258+ernest-tyunyan@users.noreply.github.com>
2023-02-26 13:24:22 +0300

    3. Арифметические операции. [#185]

--- src/main/java/ru/job4j/Calculator.java
+++ src/main/java/ru/job4j/Calculator.java
@@ -2,12 +2,12 @@ package ru.job4j;
 
 public class Calculator {
     public static void main(String[] args) {
+
         int one = 1;
         int two = 2;
         int four = 4;
         int five = 5;
         int six = 6;
-
 
         int onePlusTwo = one + two;
 
