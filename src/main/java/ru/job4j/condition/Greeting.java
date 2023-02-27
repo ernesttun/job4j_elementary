8c033a601ea2e8866b7eb80b5fc9946302ceec24
ernest-tyunyan <100211258+ernest-tyunyan@users.noreply.github.com>
2023-02-27 21:15:57 +0300

    9.1. String [#237407 #395318]

--- src/main/java/ru/job4j/condition/Greeting.java
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -5,9 +5,9 @@ public class Greeting {
     public static void main(String[] args) {
         String idea = "I like Java! ";
         System.out.println(idea);
-        idea += "But I am a newbie. ";
+        idea = idea + "But I am a newbie. ";
         int year = 2023;
-        idea += year;
+        idea = idea + year;
         System.out.println(idea);
     }
 }
