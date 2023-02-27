25dce290dcc53baa516e287aa3dfb41eed365383
ernest-tyunyan <100211258+ernest-tyunyan@users.noreply.github.com>
2023-02-27 21:33:59 +0300

    5. Конвертер валюты [#41583 #395296]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -7,14 +7,14 @@ public class Converter {
         return result;
     }
 
-    public static float rubleToDollar(int ruble) {
+    public static float rubleToDollar(float ruble) {
         float result = ruble / 60;
         return result;
     }
 
     public static void main(String[] args) {
         float euro = Converter.rubleToEuro(140);
-        float dollar = Converter.rubleToDollar(240);
+        float dollar = Converter.rubleToDollar(420);
         System.out.println("140 ruble are " + euro + " euro");
         System.out.println("420 ruble are " + dollar + " dollar");
     }
