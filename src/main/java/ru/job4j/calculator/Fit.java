67b2cbade5a8505264944cbf7a35a0ec366f26fc
ernest-tyunyan <100211258+ernest-tyunyan@users.noreply.github.com>
2023-02-28 19:42:45 +0300

    6. Идеальный вес [#41584 #395299]

--- src/main/java/ru/job4j/calculator/Fit.java
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -12,7 +12,7 @@ public class Fit {
     }
 
     public static void main(String[] args) {
-        short height = 173;
+        short height = 172;
         double man = Fit.manWeight(height);
         double woman = Fit.womanWeight(height);
         System.out.println("My 173 is " + man);
