3dd901f47a1767ff9b7a4d4a55caef2ff98a65a4
ernest-tyunyan <100211258+ernest-tyunyan@users.noreply.github.com>
2023-03-04 21:47:51 +0300

    7. Расстояние между точками в системе координат [#188 #395307]

---
+++ src/test/java/ru/job4j/condition/DummyBotTest.java
@@ -0,0 +1,32 @@
+package ru.job4j.condition;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DummyBotTest {
+
+    @Test
+    public void whenGreetBot() {
+        String in = "Hi, Bot.";
+        String result = DummyBot.answer(in);
+        String excepted = "Hi, SmartAss.";
+        assertThat(result).isEqualTo(excepted);
+    }
+
+    @Test
+    public void whenByeBot() {
+        String in = "Bye.";
+        String result = DummyBot.answer(in);
+        String excepted = "See you later.";
+        assertThat(result).isEqualTo(excepted);
+    }
+
+    @Test
+    public void whenUnknownBot() {
+        String in = "How are you ?";
+        String result = DummyBot.answer(in);
+        String excepted = "I don't know. Please, ask another question.";
+        assertThat(result).isEqualTo(excepted);
+    }
+}
