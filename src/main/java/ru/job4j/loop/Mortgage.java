902ad30f1ea55d607f2e9df441a1987b74fc396b
ernest-tyunyan <100211258+ernest-tyunyan@users.noreply.github.com>
2023-03-11 20:59:42 +0300

    5.7. Ипотека [#156315]

---
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,16 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public static int year(double amount, int salary, double percent) {
+        int year = 0;
+        percent = percent / 100 + 1;
+        while (amount > 0) {
+            double amountWithPercent = amount * percent;
+            amount = amountWithPercent - salary;
+            year++;
+        }
+        return year;
+    }
+
+}
+
