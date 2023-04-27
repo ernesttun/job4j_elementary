123fd14645c1efbd6f879a3cbbbcbe9162107994
ernest-tyunyan <100211258+ernest-tyunyan@users.noreply.github.com>
2023-04-27 23:18:01 +0300

    2.0. Рефакторинг - Расстояние между точками. [#122587]

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -2,13 +2,27 @@ package ru.job4j.condition;
 
 public class Point {
 
+    private int x;
+
+    private int y;
+
+    public Point(int first, int second) {
+        this.x = first;
+        this.y = second;
+    }
+
+    public static void main(String[] args) {
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        System.out.println(a.distance(b));
+    }
+
     public static double distance(int x1, int y1, int x2, int y2) {
         double rsl = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
         return rsl;
     }
 
-    public static void main(String[] args) {
-        double rsl = Point.distance(0, 0, 2, 0);
-        System.out.println("result (0, 0) to (2, 0) " + rsl);
+    public double distance(Point that) {
+        return Math.sqrt(Math.pow(this.x - that.x, 2)) + Math.sqrt(Math.pow(this.y - that.y, 2));
     }
 }
--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -1,40 +1,36 @@
 package ru.job4j.condition;
 
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class PointTest {
 
     @Test
     public void when00to20then2() {
         double excepted = 2;
-        int x1 = 0;
-        int x2 = 0;
-        int y1 = 2;
-        int y2 = 0;
-        double rsl = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(excepted, rsl, 0.01);
+        Point a = new Point(0, 0);
+        Point b = new Point(2, 0);
+        double rsl = a.distance(b);
+        assertThat(rsl).isEqualTo(excepted);
     }
 
     @Test
     public void when21to21then0() {
-        double excepted = 1;
-        int x1 = 0;
-        int x2 = 0;
-        int y1 = 1;
-        int y2 = 0;
-        double rsl = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(excepted, rsl, 0.01);
+        double excepted = 0;
+        Point a = new Point(2, 1);
+        Point b = new Point(2, 1);
+        double rsl = a.distance(b);
+        assertThat(rsl).isEqualTo(excepted);
     }
 
     @Test
-    public void when64To54then5() {
-        double excepted = 5;
-        int x1 = 0;
-        int x2 = 0;
-        int y1 = 5;
-        int y2 = 0;
-        double rsl = Point.distance(x1, y1, x2, y2);
-        Assert.assertEquals(excepted, rsl, 0.01);
+    public void when64To54then1() {
+        double excepted = 1;
+        Point a = new Point(6, 4);
+        Point b = new Point(5, 4);
+        double rsl = a.distance(b);
+        assertThat(rsl).isEqualTo(excepted);
     }
+
 }
